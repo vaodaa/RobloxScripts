2cc6288042bc19c3cb88520488caae87a75e4cef
Pylinear <70437477+Pylinear@users.noreply.github.com>
2020-09-19 17:10:40 +0100

    Summer Update

---
+++ BuyAvailableLimitedItems.lua
@@ -0,0 +1,45 @@
+local purchases = Instance.new("IntValue")
+purchases.Parent = game.Players.LocalPlayer
+
+repeat 
+local ohString1 = "Food"
+local ohTable2 = {
+	["Name"] = "Orange Juice",
+	["Shop"] = "Summer"
+}
+
+workspace.CommunicationRelays.Transaction.BuyItem:InvokeServer(ohString1, ohTable2)
+
+local ohString1 = "Food"
+local ohTable2 = {
+	["Name"] = "Limeade",
+	["Shop"] = "Summer"
+}
+
+workspace.CommunicationRelays.Transaction.BuyItem:InvokeServer(ohString1, ohTable2)
+
+local ohString1 = "Food"
+local ohTable2 = {
+	["Name"] = "Lemonade",
+	["Shop"] = "Summer"
+}
+
+workspace.CommunicationRelays.Transaction.BuyItem:InvokeServer(ohString1, ohTable2)
+
+local ohString1 = "Food"
+local ohTable2 = {
+	["Name"] = "Watermelon",
+	["Shop"] = "Summer"
+}
+
+workspace.CommunicationRelays.Transaction.BuyItem:InvokeServer(ohString1, ohTable2)
+
+local ohString1 = "Food"
+local ohTable2 = {
+	["Name"] = "Popsicle",
+	["Shop"] = "Summer"
+}
+
+workspace.CommunicationRelays.Transaction.BuyItem:InvokeServer(ohString1, ohTable2)
+purchases.Value += 1
+until purchases.Value == 50
